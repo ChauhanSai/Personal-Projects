bf88aaa8b38cc6402a2e98d9e830f64e187ba08d
ChauhanSai <chauhansaimc@gmail.com>
2021-06-07 22:01:35 -0500

    210607
    Python:
    Basics.py

---
+++ Python/Python with Py/Basics.py
@@ -0,0 +1,18 @@
+#Printing a string
+print("string")
+#Printing a integer(no floats)
+print(5)
+#Creating a variable
+num = "10"
+#Printing a variable(w/ int function to convert string to int)
+print(int(10))
+#Math
+num = 10
+print(10+num)
+print(10-num)
+print(10*num)
+print(10/num)
+print((10/num)*10)
+#Boolean
+print(True)
+print(True and False)
