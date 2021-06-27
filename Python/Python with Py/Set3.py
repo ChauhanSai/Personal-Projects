0ac6628b2df109fd2ec09fdfa346abc508073e40
ChauhanSai <chauhansaimc@gmail.com>
2021-06-26 23:51:06 -0500

    210626
    Python:
    Set3.py

---
+++ Python/Python with Py/Set3.py
@@ -0,0 +1,18 @@
+## For Loops
+for i in range(3):
+    print("Hello")
+shoppingList = ["Apples", "Bananas", "Oranges"]
+for i in shoppingList:
+    print(i)
+
+## While Loops
+price = 4
+while price > 0:
+    print(price)
+    price -= 1
+while True:
+    if price == 0:
+        break
+    else:
+        price = 4
+print(price+1)
