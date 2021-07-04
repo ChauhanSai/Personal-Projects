7455fad4b36930ef5d4641ded86a2576ce6b3d28
ChauhanSai <chauhansaimc@gmail.com>
2021-07-04 12:53:33 -0500

    210704
    [git-release-api Release](https://github.com/ChauhanSai/Script-Projects/releases/tag/r1)

---
+++ Python/git-release-api/Example-DownloadsApi.py
@@ -0,0 +1,6 @@
+import gitReleaseDownloads
+
+total = 0
+
+total = gitReleaseDownloads.countDownloads("ChauhanSai/Plasticator-Texture-Pack", total)
+total = gitReleaseDownloads.countDownloads("ChauhanSai/Cape-Pack", total)
