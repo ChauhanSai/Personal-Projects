72dce720b2e262a168c2412862ae1e3b3a77f5ed
ChauhanSai <chauhansaimc@gmail.com>
2021-07-16 15:17:00 -0500

    210716c
    Python:
    IFrecordKML
    -Merged python scripts into 1(Reduces connection lag)
    -Added description of aircraft to path
    -Creates a new file each time

--- Python/IFtoKML/IFrecordKML.py
+++ Python/IFtoKML/IFrecordKML.py
@@ -1,15 +1,26 @@
-import getIFApi
-import pytz
+from IFConnectOld import *
 from datetime import datetime
+import pytz
 import time
-import threading as th
 import keyboard
+
+def loc():
+    state = send_command("airplane.getstate", [], await_response=True)
+    print()
+    state = json.loads(state)['Location']
+
+    altitude=state['AltitudeLight']/17.4638
+    latitude=state['Latitude']
+    longitude=state['Longitude']
+
+    return [altitude,latitude,longitude]
 
 if __name__ == '__main__':
     breakLetter = " "
-    
     dateStart = datetime.now().strftime("%m/%d/%y")
-    kml = open('myIFflight.kml', 'w')
+    fileTime = datetime.now().strftime("%y%m%d %H.%M.%Sz")
+    fileName = "IF {}.kml".format(fileTime)
+    kml = open(fileName, 'w')
     kml.write('<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">\n<Document>\n\t<name>Infinite Flight ')
     kml.write(dateStart)
     kml.write('</name>')
@@ -19,14 +30,21 @@ if __name__ == '__main__':
     kml.write('\n\t<Placemark id="b639b753">\n\t\t<name>')
     kml.write(airport1)
     kml.write('</name>\n\t\t<Point>\n\t\t\t<coordinates>')
-    airport1Loc = getIFApi.loc()
+    airport1Loc = loc()
     kml.write(str(airport1Loc[2])) ##Longitude
     kml.write(',')
     kml.write(str(airport1Loc[1])) ##Latitude
     kml.write(',0')
     kml.write('</coordinates>\n\t\t</Point>\n\t</Placemark>')
     
-    kml.write('\n\t<Placemark id="02bdda12">\n\t\t<name>flight</name>\n\t\t<description></description>\n\t\t<gx:Track>\n\t\t\t<extrude>1</extrude>\n\t\t\t<tessellate>1</tessellate>\n\t\t\t<altitudeMode>absolute</altitudeMode>')
+    kml.write('\n\t<Placemark id="02bdda12">\n\t\t<name>flight</name>\n\t\t<description>')
+
+    airplane = send_command("airplane.getinfo", [], await_response=True)
+    airplane = json.loads(airplane)['Name']
+    kml.write('Aircraft: ')
+    kml.write(airplane)
+
+    kml.write('</description>\n\t\t<gx:Track>\n\t\t\t<extrude>1</extrude>\n\t\t\t<tessellate>1</tessellate>\n\t\t\t<altitudeMode>absolute</altitudeMode>')
 
     ##Loop every second to add data to path
     timeStart = time.time()
@@ -45,7 +63,7 @@ if __name__ == '__main__':
         kml.write('Z</when>')
         if keyboard.is_pressed(breakLetter): break
 
-        temp = getIFApi.loc()
+        temp = loc()
         if keyboard.is_pressed(breakLetter): break
         kml.write('\n\t\t\t<gx:coord>')
         if keyboard.is_pressed(breakLetter): break
@@ -73,7 +91,7 @@ if __name__ == '__main__':
     kml.write('\n\t<Placemark id="58adb2d1">\n\t\t<name>')
     kml.write(airport2)
     kml.write('</name>\n\t\t<Point>\n\t\t\t<coordinates>')
-    airport2Loc = getIFApi.loc()
+    airport2Loc = loc()
     kml.write(str(airport2Loc[2])) ##Longitude
     kml.write(',')
     kml.write(str(airport2Loc[1])) ##Latitude
@@ -89,4 +107,4 @@ if __name__ == '__main__':
     kml.write('\n</Document>\n</kml>')
     kml.close()
 
-    print('\n\nmyIFFlight.KML sucessfully recorded')
+    print('\n\n',fileName,' sucessfully recorded')
