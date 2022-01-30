d32682535e935bcef1a29c824114636983c6f33f
ChauhanSai <chauhansaimc@gmail.com>
2022-01-29 21:10:41 -0600

    210129
    Python:
    pyinstaller.py

--- Python/pyinstaller.py
+++ Python/pyinstaller.py
@@ -1,12 +1,53 @@
-import os
-pyinstaller -F --icon "icon.ico" --onefile xxx.py
+import subprocess
 
-dir = input(".Py Directory: ")
-file = dir[dir.rindex('\\')+1:]
-dir = dir[:dir.rindex('\\')]
 
-os.chdir(dir)
-cmd = ''
-cmd += 'cmd /k "'
-cmd +=
-os.system('cmd /k "Your Command Prompt Command"')
+# pyinstaller -F --icon "icon.ico" --onefile xxx.py
+
+def pyinstallerIcon(loc, icon):
+    """
+    Takes directory, *loc*, and uses *icon* as icon.
+    :type loc: basestring
+    :type icon: basestring
+    """
+    file, dir = getDir(loc)
+    args = ['pyinstaller', '-F', '--icon', icon, '--onefile', file]
+    print(' '.join(args))
+    subprocess.run(args, cwd=dir)
+
+
+def pyinstaller(loc):
+    """
+    Takes directory, *loc*, and uses 'icon.ico' as icon.
+    :type loc: basestring
+    """
+    file, dir = getDir(loc)
+    args = ['pyinstaller', '-F', '--icon', '"icon.ico"', '--onefile', file]
+    print(' '.join(args))
+    subprocess.run(args, cwd=dir)
+
+
+def getDir(loc):
+    """
+    Takes full directory returns file & working directory.
+    :rtype: tuple
+    :type loc: basestring
+    """
+    file = loc[loc.rindex('\\') + 1:]
+    dir = loc[:loc.rindex('\\') + 1]
+
+    return file, dir
+
+
+dirInput = input("Relative Python\\\\ Directory: ")
+icoInput = input("Icon File (Empty for icon.ico): ")
+if icoInput == '':
+    pyinstaller(dirInput)
+else:
+    pyinstallerIcon(dirInput, icoInput)
+
+again = input("Run again? (true/false): ")
+if again:
+    if icoInput == '':
+        pyinstaller(dirInput)
+    else:
+        pyinstallerIcon(dirInput, icoInput)
